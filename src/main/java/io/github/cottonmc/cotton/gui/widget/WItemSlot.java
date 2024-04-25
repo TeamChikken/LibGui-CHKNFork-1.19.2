5b3fa9945f212e20b1cce3ccf28b4959b0791090
Slayer5934 <slayer5934@protonmail.com>
2024-04-25 07:19:43 -0400

    Revert to 6.4.0 1.19.2 and fix server crashes.

--- src/main/java/io/github/cottonmc/cotton/gui/widget/WItemSlot.java
+++ src/main/java/io/github/cottonmc/cotton/gui/widget/WItemSlot.java
@@ -78,7 +78,7 @@ public class WItemSlot extends WWidget {
 	private final List<ValidatedSlot> peers = new ArrayList<>();
 	@Nullable
 	@Environment(EnvType.CLIENT)
-	private BackgroundPainter backgroundPainter = null;
+	private BackgroundPainter backgroundPainter;
 	@Nullable
 	private Icon icon = null;
 	private Inventory inventory;
